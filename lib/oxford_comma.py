f4ca42bf42edae5729e8f3e1bc878128cf9b62e9
SengokuGarp <garpirungu@gmail.com>
2024-03-24 11:36:31 +0300

    Initial commit

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,8 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(elements):
+    if len(elements) == 1:
+        return elements[0]
+    elif len(elements) == 2:
+        return " and ".join(elements)
+    else:
+        comma_separated = ", ".join(elements[:-1])
+        return f"{comma_separated}, and {elements[-1]}"
